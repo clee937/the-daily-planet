88de8cd446d182ce7c45e5356fd1631bde4cd57d
Nayna <mittunayna@gmail.com>
2026-06-24 11:57:08 +0100

    added update user method in user controller

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -17,8 +17,37 @@ function create(req, res) {
     });
 }
 
+async function update(req, res) {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (req.body.email) {
+      user.email = req.body.email;
+    }
+
+    if (req.body.password) {
+      user.password = req.body.password;
+      // pre('save') hook will hash it
+    }
+
+    await user.save();
+
+    res.status(200).json({
+      message: "User updated",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ message: "Something went wrong" });
+  }
+}
+
 const UsersController = {
   create: create,
+  update: update
 };
 
 module.exports = UsersController;
