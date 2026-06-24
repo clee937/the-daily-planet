0c7892b0a242d55cecd2929a91240313a768d87f
Humaira-Hossain <humairahossain@hotmail.com>
2026-06-24 13:16:59 +0100

    more safety config, fix bug

--- api/services/geminiService.js
+++ api/services/geminiService.js
@@ -1,19 +1,40 @@
-const { GoogleGenAI } = require("@google/genai");
+const { GoogleGenAI, HarmCategory, HarmBlockThreshold } = require("@google/genai");
 // import { setTimeout } from 'timers/promises';
 
 const ai = new GoogleGenAI({
     apiKey: process.env.GEMINI_API_KEY,
 });
 
-// creates a chat session where gemini remembers the history
-const chatSession = ai.chats.create({
-    model: "gemini-2.5-flash-lite",
-    config: {
-        systemInstruction: `You are a friendly space expert called Rover who is an astronaut dog.
-        Please provide answers in 100 words or less or provide more exaplanation where the user asks for it.
+// safety settings and system instructions
+const aiConfig = {
+    safetySettings: [
+        {
+            category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+            threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+        },
+        {
+            category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+            threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+        },
+        {
+            category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+            threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+        },
+        {
+            category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+            threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+        }
+    ],
+    systemInstruction: `You are a space expert called Rover who is an astronaut dog scientist.
+        Please provide answers in 50 words or less or provide more exaplanation where the user asks for it.
         If the user asks something not space related or something related to feeling low or sad,
         please answer with "I'm sorry, I am only able to engage with space related questions"`,
-    }
+};
+
+// creates a chat session where gemini remembers the history
+const chatSession = ai.chats.create({
+    model: "gemini-3.1-flash-lite", 
+    config: aiConfig
 });
 
 // sends message in the chat
@@ -36,8 +57,8 @@ async function askGemini(prompt) {
                 waitTime *= 2; 
             }
         }
-        return "Houston we have a problem, the space station is experiencing high traffic. Please try again!";
     };
+    return "Houston we have a problem, the space station is experiencing high traffic. Please try again!";
 };
 
 module.exports = {
